09c9021851d02a71fed90e7071fb1eeb4230406e
dziebart <denni.z@gmx.net>
2020-08-17 20:59:21 +0200

    Removed Exception when not all files were found. Proceeding with the values we have

--- slave.py
+++ slave.py
@@ -165,31 +165,26 @@ class FetchSlave:
             dieharder = DieHarderWrapper(self.hello_random_filename)
             dieharder.execute_all_tests()
             hello_random_results = dieharder.get_results()
-        else:
-            raise Exception("File "+self.hello_random_filename+" not found.")
+
 
         if os.path.exists(self.session_random_filename) and self.session_id_random is not None \
                 and len(self.session_id_random) > 0:
             dieharder = DieHarderWrapper(self.session_random_filename)
             dieharder.execute_all_tests()
             session_random_results = dieharder.get_results()
-        else:
-            raise Exception("File "+self.session_random_filename+" not found.")
+
 
         if os.path.exists(self.iv_random_filename) and self.iv_random is not None and len(self.iv_random) > 0:
             dieharder = DieHarderWrapper(self.iv_random_filename)
             dieharder.execute_all_tests()
             iv_random_results = dieharder.get_results()
-        else:
-            raise Exception("File "+self.iv_random_filename+" not found.")
+
 
         if os.path.exists(self.complete_random_filename) and self.complete_sequence is not None \
                 and len(self.complete_sequence) > 0:
             dieharder = DieHarderWrapper(self.complete_random_filename)
             dieharder.execute_all_tests()
             complete_random_results = dieharder.get_results()
-        else:
-            raise Exception("File "+self.complete_random_filename+" not found.")
 
         self.stat_results = StatisticalResults(self.host_name, self.previous_results, self.template_percentage_map,
                                                hello_random_results, session_random_results,
